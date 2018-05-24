d157b81aa421dad3ea1202b35abf117436fe825c
fabien terrasse <fabien.terrasse.pf1@gmail.com>
2018-05-24 16:30:58 +0200

    Squelette de l'application

---
+++ src/main/java/co/simplon/nutrapp/nutrapp_project/NutrappProjectApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.nutrapp.nutrapp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrappProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrappProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/co/simplon/nutrapp/nutrapp_project/NutrappProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package co.simplon.nutrapp.nutrapp_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NutrappProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
